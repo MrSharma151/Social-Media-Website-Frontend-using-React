75a2e63cb2ecfa3a7be7b23a4a5c46fb6c69c5d0
Rohit Sharma <rhs.rohitsharma@gmail.com>
2022-10-17 14:09:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import "./App.css"
+import "./App.css";
+import Auth from "./pages/Auth/Auth";
+import Home from "./pages/home/Home";
+import Profile from "./pages/Profile/Profile";
 function App() {
   return (
     <div className="App">
-        Subscribe Zainkeeps code
+      <div className="blur" style={{ top: "-18%", right: "0" }}></div>
+      <div className="blur" style={{ top: "36%", left: "-8rem" }}></div>
+      <Home />
+      {/* <Profile /> */}
+      {/* <Auth /> */}
     </div>
   );
 }
